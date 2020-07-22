fb2f1d43246b6ca568134346bb6b088fc828a104
Valerio <valerio.modesti@gmail.com>
2020-07-22 18:38:43 +0200

    tentativo di ammenda

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-//faccio apparire il menu
+//faccio apparire il menu con .show
 
-$('.fa-bars').click(function(){
-    $('.hamburger-menu').show('fast');
+// $('.header-right .fa-bars').click(function(){
+//     $('.hamburger-menu').show('fast');
+// });
+//
+// //faccio scomparire il menu con .hide
+// $('.close').click(function(){
+//     $('.hamburger-menu').hide('fast');
+// });
+
+// 2a soluzione
+//aggiungo la classe active ad hamburger-menu con addClass
+$('.header-right .fa-bars').click(function(){
+    $('.hamburger-menu').addClass('active');
 });
 
-//faccio scomparire il menu
+//rimuovo la classe active ad hamburger-menu con removeClass
 $('.close').click(function(){
-    $('.hamburger-menu').hide('fast');
+    $('.hamburger-menu').removeClass('active');
 });
