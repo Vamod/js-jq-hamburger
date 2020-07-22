62d150d1178a73667100f7b9becf3d8404fdc9be
Valerio <valerio.modesti@gmail.com>
2020-07-22 15:59:53 +0200

    init

---
+++ main.js
@@ -0,0 +1,10 @@
+//faccio apparire il menu
+
+$('.fa-bars').click(function(){
+    $('.hamburger-menu').show('fast');
+});
+
+//faccio scomparire il menu
+$('.close').click(function(){
+    $('.hamburger-menu').hide('fast');
+});
